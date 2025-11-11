a6d5cc04197c74ad2d95d47ec0611b76ccf068d4
shamim-732 <babushamim262@gmail.com>
2025-11-11 17:10:04 +0600

    fix: deployment ready app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,7 @@ import cors from 'cors';
 
 
 const app = express();
-
-
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
@@ -17,6 +16,6 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/uploads', express.static('uploads'));
 
-app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(" Server is running on PORT:", PORT);
 });
