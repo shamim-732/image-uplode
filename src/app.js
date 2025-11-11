f8f93ef037433d5119b37a6828d54f498db89d5b
shamim-732 <babushamim262@gmail.com>
2025-11-11 17:19:43 +0600

    deployment ready backend

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import path from 'path';
-
-
+ 
+ 
 import userRoutes from './route/userRoutes.js'
-
+ 
 import cors from 'cors';
-
-
+ 
+ 
 const app = express();
-const PORT = process.env.PORT || 5000;
+ 
+ 
 app.use(cors());
-
+ 
 app.use(express.json());
-
+ 
 app.use('/api/user', userRoutes);
 app.use('/uploads', express.static('uploads'));
-
-app.listen(PORT,'0.0.0.0', () => {
-  console.log(" Server is running on PORT:", PORT);
+ 
+app.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
 });
